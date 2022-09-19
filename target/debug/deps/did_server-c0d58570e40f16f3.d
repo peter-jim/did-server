d6067dd6a42e33fbff77749951fcb80951e45744
Peter-jim <835440306@qq.com>
2022-09-19 15:21:28 +0800

    .gitignore重写缓存成功

---
+++ target/debug/deps/did_server-c0d58570e40f16f3.d
@@ -0,0 +1,5 @@
+/root/did-server/target/debug/deps/did_server-c0d58570e40f16f3: src/main.rs
+
+/root/did-server/target/debug/deps/did_server-c0d58570e40f16f3.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/did-server.d
@@ -0,0 +1,1 @@
+/root/did-server/target/debug/did-server: /root/did-server/src/main.rs
